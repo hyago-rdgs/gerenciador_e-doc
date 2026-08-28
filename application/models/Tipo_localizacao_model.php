abe502e484c724ce38c223721a3796a76156ed7c
Hyago Rodrigues <hyagof16@gmail.com>
2026-08-28 12:31:51 -0300

    feat: adiciona busca de tipo de localização por chave

--- application/models/Tipo_localizacao_model.php
+++ application/models/Tipo_localizacao_model.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Tipo_localizacao_model extends CI_Model
 {
     private $tabela = 'tipos_localizacao';
+
     public function __construct()
     {
         parent::__construct();
@@ -23,6 +24,21 @@ class Tipo_localizacao_model extends CI_Model
     public function buscar_por_codigo($codigo)
     {
         $this->db->where('codigo', $codigo);
+        $this->db->where('exclusao IS NULL', NULL, FALSE);
+
+        $query = $this->db->get($this->tabela, 1);
+        return $query->row_array();
+    }
+
+    public function buscar_por_chave($chave)
+    {
+        $chave = trim((string) $chave);
+
+        if ($chave === '') {
+            return FALSE;
+        }
+
+        $this->db->where('chave', $chave);
         $this->db->where('exclusao IS NULL', NULL, FALSE);
 
         $query = $this->db->get($this->tabela, 1);
